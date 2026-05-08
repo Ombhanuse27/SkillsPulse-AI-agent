ee4d193854117b8fc7195420f5da94a5f70a5b8b
Ombhanuse27 <ombhanuse364@gmail.com>
2026-05-09 00:46:51 +0530

    final nextjs16 config fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ const nextConfig = {
   output: "standalone",
 
   // strictly ignore errors to pass the build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+ 
   typescript: {
     ignoreBuildErrors: true,
   },
